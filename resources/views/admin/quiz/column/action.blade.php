a6e3b4eb1ca05a15adba7fa9d831cba5f55ad34d
Moh Ibnu Abdurrohman Sutio <ibnuabdurrohmansutio@gmail.com>
2023-07-31 18:13:39 +0700

    update: add link to create or edit question

--- resources/views/admin/quiz/column/action.blade.php
+++ resources/views/admin/quiz/column/action.blade.php
@@ -2,6 +2,8 @@
     <x-edit-button title="Edit" route="{{ route('admin.quiz.edit', [$data->course_chapter_id, $data->id]) }}" />
     @if ($data->is_active)
         <x-delete-button onclick="destroy('{{ $data->id }}', '{{ $data->title }}')" />
+        <x-link-button color="dark" title="{{ $data->questions->count() ? 'Lihat Soal' : 'Tambah Soal' }}"
+            route="" />
     @else
         <x-restore-button onclick="restore('{{ $data->id }}', '{{ $data->title }}')" />
     @endif
