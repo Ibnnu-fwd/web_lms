e7a2b4f71615c4d1a3552195c914eb48de8b36b9
Moh Ibnu Abdurrohman Sutio <ibnuabdurrohmansutio@gmail.com>
2023-08-08 19:32:03 +0700

    delete sub chapter view

--- resources/views/admin/course_sub_chapter/column/action.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="flex items-center">
-    <x-edit-button route="{{ route('admin.course-sub-chapter.edit', [$data->course_chapter_id, $data->id]) }}" />
-    <x-delete-button onclick="destroy('{{ $data->id }}', '{{ $data->title }}')" />
-</div>
--- resources/views/admin/course_sub_chapter/column/file.blade.php
+++
@@ -1,5 +0,0 @@
-@if ($data->file !== null)
-    <ion-icon name="checkmark-circle" class="text-success w-5 h-5"></ion-icon>
-@else
-    <ion-icon name="close-circle" class="text-primary w-5 h-5"></ion-icon>
-@endif
